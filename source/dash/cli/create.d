3484fd129e5ff29e60bfa4e5112ad1a197e3214f
Colden Cullen <ColdenCullen@gmail.com>
2014-05-17 15:43:58 -0400

    Create is now stable, and creates a compiling project. Closes #2.

--- source/dash/cli/create.d
+++ source/dash/cli/create.d
@@ -1,7 +1,7 @@
 module dash.cli.create;
 import dash.cli.utility;
 
-import std.file, std.path;
+import std.algorithm, std.file, std.path, std.zip, std.stream;
 
 void createProject( string gameDir )
 {
@@ -10,4 +10,25 @@ void createProject( string gameDir )
         gameDir.mkdirRecurse();
 
     // Unzip empty game to new folder.
+    auto zr = new ZipArchive( thisExePath.dirName.buildNormalizedPath( "empty-game.zip" ).read() );
+
+    // Extract the empty-game.zip template.
+    foreach( ArchiveMember de; zr.directory )
+    {
+        // Ignore folders.
+        if( de.name[ $-1 ].among( '/', '\\' ) )
+            continue;
+
+        // Make sure folder exists.
+        auto absPath = de.name.inGame( gameDir );
+        if( !absPath.dirName.exists )
+            absPath.dirName.mkdirRecurse();
+
+        // Extract the file.
+        auto f = new File( absPath, FileMode.OutNew );
+        zr.expand( de );
+        f.write( de.expandedData );
+        f.flush();
+        f.close();
+    }
 }
